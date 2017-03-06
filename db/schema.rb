f3f096eac2737d8927fd27b16ec5b88b91a56c92
Jim Padilla <jp_code2013@yahoo.com>
2017-03-06 10:46:08 -0800

    Update Rails to 4.2.6

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20131103173130) do
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
