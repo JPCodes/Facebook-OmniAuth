cd84789fb9fb3732abe80078be4e713bb07bffcf
Jim Padilla <jp_code2013@yahoo.com>
2017-03-06 11:03:41 -0800

    Update user model bug fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '', '', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+  provider :facebook, 'app_id', 'app_secret', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
 end
